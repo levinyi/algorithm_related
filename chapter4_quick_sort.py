df1156039ca3678c04a8b4c7d0ba65332e224fd8
Levin <dushiyi319@163.com>
2018-01-05 15:49:45 +0800

    add some new

--- chapter4_quick_sort.py
+++ chapter4_quick_sort.py
@@ -1,11 +1,19 @@
 
 def sum2(alist):
-	if len(alist) == 0:
-		pass
-	if len(alist) == 1:
+	if len(alist) < 2:
 		return alist
 	else:
 		for i in alist:
 			return alist[0]+ sum2(alist[1:])
 
-print(sum2([2,4,6]))
+# print(sum2([2,4,6]))
+
+def quicksort(array):
+	if len(array) < 2:
+		return array
+	else:
+		pivot = array[0]
+		less = [i for i in array[1:] if i <= pivot]
+		greater = [i for i in array[1:] if i > pivot]
+		return quicksort(less)+[pivot] +quicksort(greater)
+print quicksort([10,5,2,3])
