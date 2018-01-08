4bf35d0f7555a713554b7c4b2d13004900e23d08
Levin <dushiyi319@163.com>
2018-01-08 14:29:24 +0800

    unfinish

---
+++ chapter5.dict.py
@@ -0,0 +1,19 @@
+voted = {}
+def check_voter(name):
+	if voted.get(name):
+		print("kick them out!")
+	else:
+		voted[name] = True
+		print("let them vote!")
+check_voter("Tom")
+check_voter("mike")
+check_voter("mike")
+
+cache = {}
+def get_page(url):
+	if cache.get(url):
+		return cache[url]
+	else:
+		data = get_data_from_server(url)
+		cache[url] = data
+		return data
