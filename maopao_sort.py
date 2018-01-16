ff2a8def6c90c1f8dda044a261100b7a16683b21
Levin <dushiyi319@163.com>
2018-01-16 15:42:14 +0800

    test

--- maopao_sort.py
+++ maopao_sort.py
@@ -3,9 +3,14 @@
 def msort(alist):
 	number = len(alist) #5 总共有5个数
 	# 先来排序第一趟
-	for i in alist:
+	print(alist,len(alist))
+	for i in range(len(alist)-1):
+		print i
 		if alist[i] < alist[i+1]:
 			alist[i],alist[i+1] = alist[i+1],alist[i]
 			print alist
+	# alist.pop()
+	msort(alist)
 
 msort([6,1,2,7,9,3,4,5,19,8])
+
